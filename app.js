420909246593a60fd2167db8f549d075a7e5b6bb
aocirey <aocirey@qualist.com>
2020-02-06 15:31:27 +0300

    creating object structure is changed

--- app.js
+++ app.js
@@ -1,52 +1,73 @@
 var form;
 var inputs = [];
 var outputs = [];
-var objwithOrder=[];
-var objIndex = 0;
-var obj = {
-    name: "",
-    unit: "",
-    dropdown: [],
-    NumberOrText: "",
-    isMandatory: Boolean,
-    isMulti: Boolean,
-}
+var name = "";
+var unit = "";
+var dropdown = [];
+var NumberOrText = "";
+var isMandatory = Boolean;
+var isMulti = Boolean;
+var indexOfObj = -1;
+
+function createObj(name, unit, dropdown, NumberOrText, isMandatory, isMulti){
+    this.name = name;
+    this.unit= unit;
+    this.dropdown = dropdown;
+    this.NumberOrText = NumberOrText;
+    this.isMandatory = isMandatory;
+    this.isMulti = isMulti;
+};
+
+
 var tbl = document.getElementById("tbl");
 
-
-    form = document.getElementById("operation");
+form = document.getElementById("operation");
 
 form.onsubmit = function (e) {
     e.preventDefault();
-    obj.name = form.name.value;
-    obj.unit = form.unit.value;
+    indexOfObj++;
 
     if (form.mandatory.checked == true) {
-        obj.isMandatory = true;
+        isMandatory = true;
     } else {
-        obj.isMandatory = false;
+        isMandatory = false;
     }
 
     if (form.multi.checked == true) {
-        obj.isMulti = true;
+        isMulti = true;
     } else {
-        obj.isMulti = false;
+        isMulti = false;
 
     }
 
     if (form.dd.value == "dropdown") {
-        obj.dropdown.push(form.dd.value);
+        dropdown = form.dd.value;
     } else if (form.dd.value == "number") {
-        obj.NumberOrText = "number";
+        NumberOrText = "number";
     } else {
-        obj.NumberOrText = "text";
+        NumberOrText = "text";
     }
 
+    var newObj = "obj"+indexOfObj.toString();
+
+    newObj = new createObj(form.name.value,form.name.value,dropdown,isMandatory,isMulti);
+
     var newTr = document.createElement("tr");
-    var newTd = '<td>' + obj.name + '</td><td>' + obj.unit + '</td><td>' + obj.isMandatory +
-        '</td><td>' + obj.isMulti + '</td><td>' + form.dd.value + '</td>';
+    var newTd = '<td>' + newObj.name + '</td><td>' + newObj.unit + '</td><td>' + newObj.isMandatory +
+        '</td><td>' + newObj.isMulti + '</td><td>' + form.dd.value + '</td>';
     $(newTr).append(newTd);
     newTr.addEventListener("click", function () {
+         
+        for(i=0;i<$("#tbl tr").length;i++){
+            
+            if($("#tbl tr")[i].innerText==$(this)[0].innerText){
+                console.log($("#tbl tr")[i].innerText);
+                console.log($(this)[0].innerText)
+                inputs.splice(i,1);
+                console.log(inputs)
+            }
+       
+        }
         
         form.name.value = $(this.childNodes)[0].textContent;
         form.unit.value = $(this.childNodes)[1].textContent;
@@ -55,17 +76,13 @@ form.onsubmit = function (e) {
         form.dd.value = $(this.childNodes)[4].textContent;
         $(this).remove();
 
-        console.log($(this))
-
-
     });
+
     $(tbl).append(newTr);
+    
 
 
-    objwithOrder.push([obj,objIndex]);
-    console.log(objwithOrder);
-    inputs.push(obj);
-
+    inputs.push(newObj);
     console.log(inputs)
     form.reset();
 
