17749d8c81a2310b8bc7aaa10d3d37ef87ab25ba
aocirey <aocirey@qualist.com>
2020-02-06 12:54:57 +0300

    underConstruction

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var form;
 var inputs = [];
 var outputs = [];
+var objwithOrder=[];
+var objIndex = 0;
 var obj = {
     name: "",
     unit: "",
@@ -8,11 +10,11 @@ var obj = {
     NumberOrText: "",
     isMandatory: Boolean,
     isMulti: Boolean,
-
 }
 var tbl = document.getElementById("tbl");
 
-form = document.getElementById("operation");
+
+    form = document.getElementById("operation");
 
 form.onsubmit = function (e) {
     e.preventDefault();
@@ -40,14 +42,32 @@ form.onsubmit = function (e) {
         obj.NumberOrText = "text";
     }
 
-    
-    var newTd = '<tr><td>' + obj.name + '</td><td>' + obj.unit + '</td><td>' + obj.isMandatory + 
-    '</td><td>' + obj.isMulti + '</td><td>' + form.dd.value + '</td></tr>';
-    $(tbl).append(newTd);
-    newTd.
+    var newTr = document.createElement("tr");
+    var newTd = '<td>' + obj.name + '</td><td>' + obj.unit + '</td><td>' + obj.isMandatory +
+        '</td><td>' + obj.isMulti + '</td><td>' + form.dd.value + '</td>';
+    $(newTr).append(newTd);
+    newTr.addEventListener("click", function () {
+        
+        form.name.value = $(this.childNodes)[0].textContent;
+        form.unit.value = $(this.childNodes)[1].textContent;
+        form.mandatory.value = $(this.childNodes)[2].textContent;
+        form.multi.value = $(this.childNodes)[3].textContent;
+        form.dd.value = $(this.childNodes)[4].textContent;
+        $(this).remove();
+
+        console.log($(this))
 
 
+    });
+    $(tbl).append(newTr);
+
+
+    objwithOrder.push([obj,objIndex]);
+    console.log(objwithOrder);
     inputs.push(obj);
+
     console.log(inputs)
     form.reset();
+
+
 };
