4a53f3f68336ec7bb2619d23ae5fbe03bd350abb
aocirey <aocirey@qualist.com>
2020-02-13 17:33:53 +0300

    lüfullah editledi

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 (function ($) {
 
 
@@ -15,10 +14,18 @@
     ProcessParameter.prototype = {
         init: function () {
 
-            var template = `<div class="processparameter mt-3">
+            var InputType = {
+                Text: "1",
+                Number: "2",
+                Dropdown: "3",
+                Checkbox: "4"
+            }
+            var template = `
+                            <div class="processparameter dragcontent mt-3">
                                 <div class="container">
                                     <div class="row processparameter-header">
                                         <div class="col-12">
+                                        <i class="dragable fas fa-arrows-alt"></i>
                                             <span class="param-name">Add parameter</span>
                                             <div class="card-action-bar">
                                                 <a data-toggle="collapse" class="float-rigth" data-target="#collapse11">
@@ -91,7 +98,8 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>`
+                            </div>
+                        `
             var DDtemplate = `<div class="mb-2 selection-item col-12 row">
                                                         <div class="col-9">
                                                             <label>Choices</label>
@@ -127,6 +135,7 @@
                     Selection: null
                 };
             }
+
             function addProcessParameter(newParam) {
                 var processParameters = getProcessParameters();
 
@@ -151,12 +160,13 @@
                 var parmeters = getProcessParameters();
                 for (var i = 0; i < parmeters.length; i++) {
                     if (parmeters[i].Name == name) {
-                        return [i, parmeters[i]];
+                        return parmeters[i];
 
                     }
                 }
                 return false;
             }
+
             function updateProcessParameter(key, processParameter) {
                 var processParmeters = getProcessParameters();
                 for (var i = 0; i < processParmeters.length; i++) {
@@ -170,16 +180,15 @@
 
             }
 
+            function reloadProcessParameters(processParameters) {
 
+            }
 
-            var $container = this.item;
-            $container.addClass("processparameters-container");
-            $container.append($(addButtonTemplate));
-            $container.find(".new-processparameter").click(function () {
+            function addParameter(jsonData) {
                 var templateObj = $(template);
 
-                $(".collapse").hide();
-                $(".processparameter-header").show();
+                $container.find(".collapse").removeClass("show");
+                $container.find(".processparameter-header").show();
 
                 var nameInput = templateObj.find("input[name=card-mes-name-input-field]");
                 nameInput.keypress(function (e) {
@@ -191,114 +200,86 @@
 
                 templateObj.find(".cancel-processparameter").click(function () {
                     var container = $(this).closest(".processparameter");
-                    var inputs = container.find(".form-control");
-                    var changedInputs = 0;
-                    for (i = 0; i < inputs.length; i++) {
-                        if (inputs[i].value != "" && inputs[i].value != "Choose and Add Value") {
-                            //console.log(inputs[i].value)
-                            changedInputs++;
-                        }
-                    }
-                    if (changedInputs > 0) {
-                        //console.log(container.find(".form-control"));
-                        alert("değişiklik var" + changedInputs)
-                    } else {
+                    var hiddenNameInput = container.find("input[name='parameter-name']")
+                    if (!hiddenNameInput.val()) {
                         container.remove();
-                        changedInputs = 0;
+                        return;
                     }
 
+                    var existingObj = getProcessParmeterByName(hiddenNameInput.val());
+                    var newObj = getJsonForProcessParameter(container);
+                    if (JSON.stringify(existingObj) === JSON.stringify(newObj)) {
+                        $(this).closest(".collapse").toggle();
+                        $(this).closest(".processparameter").find(".processparameter-header").toggle();
+                        return;
+                    }
+
+
+
+                    Swal.fire({
+                        title: 'Değişiklikleri var!!',
+                        text: "Kaydetmek istiyormusun",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Evet'
+                    }).then((result) => {
+                        if (result.dismiss == "cancel") {
+                            container.empty();
+                            container.append(addParameter(existingObj).children());
+                            $(this).closest(".collapse").toggle();
+                            $(this).closest(".processparameter").find(".processparameter-header").toggle();
+                            return;
+
+                        } else if (result.dismiss == "continue") {
+                            return;
+                        }
+                        updateProcessParameter(hiddenNameInput.val(), newObj);
+                        hiddenNameInput.val(newObj.Name);
+
+                        Swal.fire(
+                            'Kaydedildi!',
+                            'Parametre güncellemeniz kaydedildi',
+                            'success'
+                        )
+
+                        $(this).closest(".collapse").toggle();
+                        $(this).closest(".processparameter").find(".processparameter-header").toggle();
+                    });
 
                 });
                 templateObj.find(".save-processparameter").click(function () {
                     var container = $(this).closest(".processparameter");
+                    var obj = getJsonForProcessParameter(container);
+                    var hiddenNameInput = container.find("input[name='parameter-name']");
+                    var existedName = hiddenNameInput.val();
+                    var existedItem;
+                    if (existedName && existedName == obj.Name) {
+                        existedItem = null;
+                    }
+                    else {
+                        existedItem = getProcessParmeterByName(obj.Name);
+                    }
 
-                    var inputs = container.find(".form-control");
-                    var chargedInputs = 0;
-                    for (i = 0; i < inputs.length; i++) {
-                        if (inputs[i].value != "" && inputs[i].value != "Choose and Add Value") {
-                            chargedInputs++;
-                        }
-                    };
-                    if (chargedInputs < inputs.length) {
+                    if (existedItem) {
                         Swal.fire({
                             icon: 'error',
                             title: 'Oops...',
-                            text: 'Lütfen bütün inputları doldurunuz'
+                            text: 'İsim, başka parametre ismi ile çakışıyor!'
                         });
-                    } else {
-
-                        var obj = createObj();
-
-                        var hiddenNameInput = container.find("input[name='parameter-name']");
-
-                        var paramName = container.find("input[name=card-mes-name-input-field]").val();
-
-                        obj.Name = paramName;
-                        obj.DisplayName = container.find("input[name=card-mes-displayname-input-field]").val();
-                        obj.Type = container.find("select[name=card-mes-select-input-field]").val();
-                        obj.Unit = container.find("input[name=card-mes-unit-input-field]").val();
-
-                        obj.Selection = [];
-                        var selections = container.find(".selections-container .selection-item input");
-                        for (var i = 0; i < selections.length; i++) {
-                            obj.Selection.push({ Value: $(selections[i]).val(), Text: $(selections[i]).val() });
-                        }
-                        obj.Required = container.find("input[name=card-mes-required-input-field]")[0].checked;
-                        obj.Clonable = container.find("input[name=card-mes-multiple-input-field]")[0].checked;
-
-
-
-                        var existedName = container.find("input[name='parameter-name']").val();
-                        var isInJson = getProcessParmeterByName(existedName)
-                        console.log(isInJson + "---------------------");
-                        if (existedName) {
-
-                            jsonOrder = isInJson[0]
-                            console.log(isInJson[1] + "is in jason")
-                            console.log(jsonOrder + "jason order");
-                           
-                            console.log(existedName + "existedname")
-                            if (!existedName) {
-                                Swal.fire({
-                                    icon: 'error',
-                                    title: 'Oops...',
-                                    text: 'İsim, başka parametre ismi ile çakışıyor!'
-                                });
-                                return
-                            } else if (existedName == isInJson[1].Name) {
-                                console.log("jsonda vardı, edit yapıldı")
-                                updateProcessParameter(existedName, obj)
-                                hiddenNameInput.val(obj.Name);
-                                $(this).closest(".collapse").toggle();
-                                $(this).closest(".processparameter").find(".processparameter-header").toggle();
-                            }
-                            if (obj.DisplayName != "") {
-                                $(this).closest(".processparameter").find(".param-name")[0].innerText = obj.DisplayName;
-                            }
-
-
-                        } else {
-                            console.log(" yeni json objesi ekledin ")
-                            addProcessParameter(obj);
-                            hiddenNameInput.val(obj.Name);
-                            $(this).closest(".collapse").toggle();
-                            if (obj.DisplayName != "") {
-                                $(this).closest(".processparameter").find(".param-name")[0].innerText = obj.DisplayName;
-                            }
-                            $(this).closest(".processparameter").find(".processparameter-header").toggle();
-                        };
-
-
-
-
+                        return
                     }
+                    if (existedName)
+                        updateProcessParameter(existedName, obj)
+                    else
+                        addProcessParameter(obj);
 
-
+                    hiddenNameInput.val(obj.Name);
+                    $(this).closest(".collapse").toggle();
+                    $(this).closest(".processparameter").find(".processparameter-header").toggle();
+                    $(this).closest(".processparameter").find(".param-name").text(obj.DisplayName);
                 });
-
-
-                //---------------------------------------------
-
                 templateObj.find(".processparameter-header .deleteParameter").on("click", function () {
                     var objName = $(this).closest(".processparameter").find("input").first().val();
                     var ProcessParameters = getProcessParameters();
@@ -317,14 +298,12 @@
 
 
                 });
-
                 templateObj.find(".processparameter-header .editParameter ").on("click", function () {
                     $(".collapse").hide();
                     $(".processparameter-header").show();
                     $(this).closest(".processparameter").find(".collapse").toggle();
                     $(this).closest(".processparameter-header").toggle();
                 });
-
                 templateObj.find("select[name=card-mes-select-input-field]").on("change", function () {
 
                     $(this).closest(".processparameter").find(".selections-container").show().find(".selection-item").remove();
@@ -335,22 +314,75 @@
                     }
                 });
 
-                function addSelection() {
-                    var selectionTemplate = $(DDtemplate);
-                    selectionTemplate.find(".add").click(function () {
-                        var tempSelection = addSelection();
-                        tempSelection.insertAfter($(this).closest(".selection-item"));
-                    });
-                    selectionTemplate.find(".remove").click(function () {
-                        $(this).closest(".selection-item").remove();
-                    });
-                    return selectionTemplate;
-                }
-                templateObj.insertBefore($(this));
-                templateObj.find(".collapse").toggle();
-                templateObj.find(".processparameter-header").toggle();
-            });
+                templateObj.find(".collapse").addClass("show");
+                templateObj.find(".processparameter-header").hide();
 
+
+
+                if (jsonData) {
+                    templateObj.find("input[name=card-mes-name-input-field]").val(jsonData.Name);
+                    templateObj.find("input[name=card-mes-displayname-input-field]").val(jsonData.DisplayName);
+                    templateObj.find("select[name=card-mes-select-input-field]").val(jsonData.Type);
+                    templateObj.find("input[name=card-mes-unit-input-field]").val(jsonData.Unit);
+
+                    if (jsonData.Type == InputType.Dropdown) {
+                        var selectionContainer = templateObj.find(".selections-container");
+                        for (var i = 0; i < jsonData.Selection; i++) {
+                            selectionContainer.append(addSelection(jsonData.Selection[i]));
+                        }
+                    }
+                    templateObj.find("input[name=card-mes-required-input-field]")[0].checked = jsonData.Required;
+                    templateObj.find("input[name=card-mes-multiple-input-field]")[0].checked = jsonData.Clonable;
+                    templateObj.find("input[name=parameter-name]").val(jsonData.Name);
+                }
+                return templateObj;
+            }
+
+            function addSelection(val) {
+                var selectionTemplate = $(DDtemplate);
+                selectionTemplate.find(".selection-item").val(val);
+                selectionTemplate.find(".add").click(function () {
+                    var tempSelection = addSelection();
+                    tempSelection.insertAfter($(this).closest(".selection-item"));
+                });
+                selectionTemplate.find(".remove").click(function () {
+                    $(this).closest(".selection-item").remove();
+                });
+                return selectionTemplate;
+            }
+
+            function getJsonForProcessParameter(container) {
+
+                var obj = createObj();
+                var paramName = container.find("input[name=card-mes-name-input-field]").val();
+
+                obj.Name = paramName;
+                obj.DisplayName = container.find("input[name=card-mes-displayname-input-field]").val();
+                obj.Type = container.find("select[name=card-mes-select-input-field]").val();
+                obj.Unit = container.find("input[name=card-mes-unit-input-field]").val();
+
+                obj.Selection = [];
+                if (obj.Type == InputType.Dropdown) {
+
+                    var selections = container.find(".selections-container .selection-item input");
+                    for (var i = 0; i < selections.length; i++) {
+                        obj.Selection.push({ Value: $(selections[i]).val(), Text: $(selections[i]).val() });
+                    }
+                }
+
+                obj.Required = container.find("input[name=card-mes-required-input-field]")[0].checked;
+                obj.Clonable = container.find("input[name=card-mes-multiple-input-field]")[0].checked;
+                return obj;
+            }
+
+            var $container = this.item;
+
+            $container.addClass("processparameters-container");
+            $container.append($(addButtonTemplate));
+            $container.find(".new-processparameter").click(function () {
+                addParameter().insertBefore($(this));
+
+            });
         },
         getData: function () {
             var processParameters = this.item.data("processparameters");
@@ -361,6 +393,8 @@
             return processParameters;
         }
     };
+
+
 
     $.fn.ProcessParameter = function (opt) {
         var args = Array.prototype.slice.call(arguments, 1);
@@ -382,9 +416,11 @@
 
 }(jQuery));
 
+$(document).ready(function () {
+    $(".cont").ProcessParameter("getData");
+    $(".cont2").ProcessParameter("getData")
 
-$(".cont").ProcessParameter("getData");
-$(".cont2").ProcessParameter("getData")
+});
 
 
 
